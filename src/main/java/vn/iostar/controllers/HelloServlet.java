669a1d841a5e87a2346924d2021e84b519f60487
finnt <finnt@FINNTRAN>
2025-08-22 16:55:58 +0700

    Login by Cookie and Session

---
+++ src/main/java/vn/iostar/controllers/HelloServlet.java
@@ -0,0 +1,37 @@
+package vn.iostar.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.http.Cookie;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet(urlPatterns = {"/hello"})
+public class HelloServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter printWriter = resp.getWriter();
+		String name = "";
+		// Nhận cookie
+		Cookie[] cookie = req.getCookies();
+		for (Cookie c : cookie) {
+			if (c.getName().equals("username")) {
+				name = c.getValue();
+			}
+		}
+
+		if (name.equals("")) {
+			// chuyển sang trang LoginServlet
+			resp.sendRedirect("/HelloServlet/login");
+		}
+		// hiển thị lên trang bằng đối tượng PrintWriter()
+		printWriter.println("Xin chao " + name);
+	}
+
+}
